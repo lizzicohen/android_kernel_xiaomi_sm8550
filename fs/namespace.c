849b6a19e59ef9516f31653660d76904d0e0e4fb
Oliwier Krawczyk <36317711+TheMysticle@users.noreply.github.com>
2026-08-19 12:01:09 +0200

    fs: Update to latest susfs v2.2.0

--- fs/namespace.c
+++ fs/namespace.c
@@ -38,7 +38,6 @@
 
 #include "pnode.h"
 #include "internal.h"
-#include <trace/hooks/blk.h>
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 extern bool susfs_is_current_ksu_domain(void);
@@ -47,7 +46,7 @@ extern struct static_key_true susfs_is_sdcard_android_data_not_decrypted;
 #define CL_COPY_MNT_NS BIT(25) /* used by copy_mnt_ns() */
 
 #endif // #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-
+#include <trace/hooks/blk.h>
 
 /* Maximum number of mounts in a mount namespace */
 unsigned int sysctl_mount_max __read_mostly = 100000;
